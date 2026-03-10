4d8550882b3fc34492a8bc443de55bc837abcca6
PhungMinhPhuc <phungminhphuc28092006@gmail.com>
2026-03-10 09:21:41 +0700

    Update

--- week3/setup_env.sql
+++ week3/setup_env.sql
@@ -3,3 +3,5 @@ DROP DATABASE IF EXISTS week3;
 
 -- Create a new database
 CREATE DATABASE week3;
+
+-- Note: After running 'setup_env.sql', you MUST switch to the "week3" database, because switching databases inside a query is not supported.
