51a13963b2b71671e7ccacba0a366216b683acf8
PhungMinhPhuc <phungminhphuc28092006@gmail.com>
2026-03-10 10:16:56 +0700

    Update

--- week3/ex03.sql
+++ week3/ex03.sql
@@ -1,6 +1,8 @@
 CREATE TABLE countries (
     country_id TEXT,
     country_name TEXT,
-    region_id TEXT
-    CONSTRAINT chk_country_name CHECK (country_namw IN (''))
-)
+    region_id TEXT,
+    CONSTRAINT chk_country_name CHECK (country_name IN ('Italy', 'Indian', 'China'))
+);
+
+SELECT * FROM countries;
